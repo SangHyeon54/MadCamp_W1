39dbe5ff92ab421230c319906b252b1f04c72ab6
Sanghyeon54 <lsh980504@kaist.ac.kr>
2020-07-08 20:56:05 +0900

    List View Init

---
+++ app/src/main/java/com/example/tap/Fragment_1.java
@@ -0,0 +1,126 @@
+package com.example.tap;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment_1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+
+
+
+public class Fragment_1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment_1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment_1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment_1 newInstance(String param1, String param2) {
+        Fragment_1 fragment = new Fragment_1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, null) ;
+
+        ArrayList contacts = new ArrayList();
+
+        AssetManager assetManager= getContext().getAssets();
+
+        try {
+            InputStream is= assetManager.open("jsons/Contact.json");
+            InputStreamReader isr= new InputStreamReader(is);
+            BufferedReader reader= new BufferedReader(isr);
+
+            StringBuffer buffer= new StringBuffer();
+            String line= reader.readLine();
+            while (line!=null){
+                buffer.append(line+"\n");
+                line=reader.readLine();
+            }
+
+            String jsonData= buffer.toString();
+
+            //json 데이터가 []로 시작하는 배열일때..
+            JSONArray jsonArray= new JSONArray(jsonData);
+
+            String s="";
+
+            for(int i=0; i<jsonArray.length();i++){
+                JSONObject jo=jsonArray.getJSONObject(i);
+
+                String name= jo.getString("name");
+                String number= jo.getString("contact");
+
+
+                //s += name+" : "+msg+"==>"+aa+","+bb+"\n";
+                contacts.add(name + " | " + number);
+            }
+
+        } catch (IOException e) {e.printStackTrace();}
+        catch (JSONException e) {e.printStackTrace(); }
+
+
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, contacts) ;
+
+        ListView listview = (ListView) view.findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/tap/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.tap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Tablayout
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("연락처"));
+        tabs.addTab(tabs.newTab().setText("갤러리"));
+        tabs.addTab(tabs.newTab().setText("----"));
+        tabs.setTabGravity(tabs.GRAVITY_FILL);
+
+        //Adapter
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        final MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), 3);
+        viewPager.setAdapter(myPagerAdapter);
+
+        //탭 선택 이벤트
+        tabs.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
+    }
+}
